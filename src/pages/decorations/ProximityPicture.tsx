66d80145265451e83183211b80f04714de3552ee
Alex Shortt <alexander.shortt@gmail.com>
2022-08-25 01:24:50 -0700

    tsc fix, yarn sync

---
+++ src/pages/decorations/ProximityPicture.tsx
@@ -0,0 +1,12 @@
+import dynamic from "next/dynamic";
+const DeveloperReality = dynamic(import("realities/DeveloperReality"), {
+  ssr: false,
+});
+const Idea = dynamic(import("ideas/decorations/ProximityPicture"), {
+  ssr: false,
+});
+export default () => (
+  <DeveloperReality>
+    <Idea />
+  </DeveloperReality>
+);
