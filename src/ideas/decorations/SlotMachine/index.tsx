53eaf4a7213dde3b4ec12bc6a487af06aedb8ac3
benjaminha14 <benjaminha14@gmail.com>
2022-09-05 03:21:27 -0700

    need to set rules for props

--- src/ideas/decorations/SlotMachine/index.tsx
+++ src/ideas/decorations/SlotMachine/index.tsx
@@ -6,44 +6,74 @@ import Button from "./components/Button";
 
 export type SlotMachineProps = {
   url?: string;
+  numReals?: number;
+  picture_a?: string;
+  picture_b?: string;
+  picture_c?: string;
+  picture_d?: string;
+  num_plays_per_day?: number;
+  num_of_icons?: number;
 } & GroupProps;
 
-// function StartSlot(props: SlotMachineProps) {
-//     retu
-// }
-const icons = [
-  "https://d27rt3a60hh1lx.cloudfront.net/images/casino/cherry.png",
-  "https://d27rt3a60hh1lx.cloudfront.net/images/casino/diamond.png",
-  "https://d27rt3a60hh1lx.cloudfront.net/images/casino/lucky7.png",
-];
-
 function randomIntFromInterval(min: number, max: number) {
-  // min and max included
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+function setWithExpiry(key: string, value: number, ttl: number) {
+  const now = new Date();
+
+  const item = {
+    value: value,
+    expiry: now.getTime() + ttl,
+  };
+  localStorage.setItem(key, JSON.stringify(item));
+}
+
+function getWithExpiry(key: string) {
+  const itemStr = localStorage.getItem(key);
+  if (!itemStr) {
+    return null;
+  }
+  const item = JSON.parse(itemStr);
+  const now = new Date();
+  if (now.getTime() > item.expiry) {
+    localStorage.removeItem(key);
+    return null;
+  }
+  return item.value;
+}
+
 export default function SlotMachine(props: SlotMachineProps) {
-  const { url = "https://muse.place/", ...rest } = props;
-  const [onClick, setOnClick] = useState(false);
+  const {
+    url = "https://muse.place/",
+    picture_a = "https://d27rt3a60hh1lx.cloudfront.net/images/casino/cherry.png",
+    picture_b = "https://d27rt3a60hh1lx.cloudfront.net/images/casino/diamond.png",
+    picture_c = "https://d27rt3a60hh1lx.cloudfront.net/images/casino/lucky7.png",
+    picture_d = "https://d27rt3a60hh1lx.cloudfront.net/images/casino/grape.png",
+    num_plays_per_day = 3,
+    num_of_icons = 4,
+    ...rest
+  } = props;
+
+  const icons = [picture_a, picture_b, picture_c, picture_d];
 
   const [visible, setVisible] = useState(false);
+  const [messageVisible, setMessageVisible] = useState(false);
   const [real1, setReal1] = useState(0);
   const [real2, setReal2] = useState(0);
   const [real3, setReal3] = useState(0);
+
   const slotGame = () => {
-    // randomize
     console.log("triggered");
     setVisible(false);
-    const real1 = randomIntFromInterval(0, icons.length - 1);
-    const real2 = randomIntFromInterval(0, icons.length - 1);
-    const real3 = randomIntFromInterval(0, icons.length - 1);
-    console.log("Real 1 : " + real1);
-    setReal1(real1);
-    setReal2(real2);
-    console.log("Real 2 : " + real2);
-    setReal3(real3);
-    console.log("Real 3 : " + real3);
-    if (real1 === real2 && real1 === real3) {
+    const real1Result = randomIntFromInterval(0, num_of_icons - 1);
+    const real2Result = randomIntFromInterval(0, num_of_icons - 1);
+    const real3Result = randomIntFromInterval(0, num_of_icons - 1);
+
+    setReal1(real1Result);
+    setReal2(real2Result);
+    setReal3(real3Result);
+    if (real1Result === real2Result && real1Result === real3Result) {
       console.log("you win");
       setVisible(true);
     } else {
@@ -52,9 +82,22 @@ export default function SlotMachine(props: SlotMachineProps) {
     }
   };
 
-  // useEffect(() =>{
-  //   slotGame
-  // },[onClick] );
+  const slotExecuteGame = () => {
+    if (
+      getWithExpiry("playsToday") == null ||
+      getWithExpiry("playsToday") < num_plays_per_day
+    ) {
+      let plays = 0;
+      if (getWithExpiry("playsToday") != null) {
+        plays = getWithExpiry("playsToday");
+      }
+      slotGame();
+      plays += 1;
+      setWithExpiry("playsToday", plays, 86400);
+    } else {
+      setMessageVisible(true);
+    }
+  };
 
   return (
     <group {...rest}>
@@ -64,7 +107,7 @@ export default function SlotMachine(props: SlotMachineProps) {
             <Image transparent src={icons[real1]} position-x={1} />
           </Suspense>
 
-          <Suspense fallback={null}>
+          <Suspense fallback={<Text> Loading</Text>}>
             <Image transparent src={icons[real2]} position-x={0} />
           </Suspense>
           <Suspense fallback={<Text> Loading</Text>}>
@@ -74,7 +117,7 @@ export default function SlotMachine(props: SlotMachineProps) {
       </group>
       <Button
         text="play"
-        onClick={() => slotGame()}
+        onClick={() => slotExecuteGame()}
         position-z={1}
         position-y={-1}
       />
@@ -86,6 +129,11 @@ export default function SlotMachine(props: SlotMachineProps) {
           position-z={1}
           onClick={() => window.open(url, "_blank")}
         />
+      ) : undefined}
+      {messageVisible ? (
+        <Text position-x={0} position-y={-0.4} position-z={1} color="red">
+          You are out of plays. Try again tomorrow.
+        </Text>
       ) : undefined}
       ;
     </group>
