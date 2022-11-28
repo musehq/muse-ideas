73483d61ad70f507515769073b9a0b7834a9709a
Alex Shortt <alexander.shortt@gmail.com>
2022-11-28 08:20:11 -0800

    remove Lost land, upgrade spacesvr to v2.7.1

--- src/pages/decorations/SlotMachine.tsx
+++ src/pages/decorations/SlotMachine.tsx
@@ -2,9 +2,7 @@ import dynamic from "next/dynamic";
 const DeveloperReality = dynamic(import("realities/DeveloperReality"), {
   ssr: false,
 });
-const Idea = dynamic(import("ideas/decorations/SlotMachine"), {
-  ssr: false,
-});
+const Idea = dynamic(import("ideas/decorations/SlotMachine"), { ssr: false });
 export default () => (
   <DeveloperReality>
     <Idea />
