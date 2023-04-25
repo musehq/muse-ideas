3c99b82770ee11696f8c31ebad66f5d7d78c4081
benjaminha14 <benjaminha14@gmail.com>
2023-04-24 20:38:42 -0700

    reorg custom ideas to ghost folder

--- src/pages/ghost/GhostDoor.tsx
+++ src/pages/ghost/GhostDoor.tsx
@@ -2,7 +2,7 @@ import dynamic from "next/dynamic";
 const DeveloperReality = dynamic(import("realities/DeveloperReality"), {
   ssr: false,
 });
-const Idea = dynamic(import("ideas/plugins/GhostDoor"), { ssr: false });
+const Idea = dynamic(import("ideas/ghost/GhostDoor"), { ssr: false });
 export default () => (
   <DeveloperReality>
     <Idea />
